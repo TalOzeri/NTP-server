7ebd022f826353bb3b7d51a6f70366e6d38b6c74
Tal Ozeri <talosozeri@gmail.com>
2025-06-17 13:40:36 +0300

    Create ntpServer.c with with basic functionalities: (creating socket and binding address to it)

---
+++ source/c/ntpServer.c
@@ -0,0 +1,74 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
+#define NTP_PORT_NUMBER 123
+#define MAX_CONNECTIONS 3
+
+void error( char* msg )
+{
+    perror( msg ); // Print the error message to stderr.
+
+    exit( 0 ); // Quit the process.
+}
+
+typedef struct
+{
+
+    uint8_t li_vn_mode;      // Eight bits. li, vn, and mode.
+    // li.   Two bits.   Leap indicator.
+    // vn.   Three bits. Version number of the protocol.
+    // mode. Three bits. Client will pick mode 3 for client.
+
+    uint8_t stratum;         // Eight bits. Stratum level of the local clock.
+    uint8_t poll;            // Eight bits. Maximum interval between successive messages.
+    uint8_t precision;       // Eight bits. Precision of the local clock.
+
+    uint32_t rootDelay;      // 32 bits. Total round trip delay time.
+    uint32_t rootDispersion; // 32 bits. Max error aloud from primary clock source.
+    uint32_t refId;          // 32 bits. Reference clock identifier.
+
+    uint32_t refTm_s;        // 32 bits. Reference time-stamp seconds.
+    uint32_t refTm_f;        // 32 bits. Reference time-stamp fraction of a second.
+
+    uint32_t origTm_s;       // 32 bits. Originate time-stamp seconds.
+    uint32_t origTm_f;       // 32 bits. Originate time-stamp fraction of a second.
+
+    uint32_t rxTm_s;         // 32 bits. Received time-stamp seconds.
+    uint32_t rxTm_f;         // 32 bits. Received time-stamp fraction of a second.
+
+    uint32_t txTm_s;         // 32 bits and the most important field the client cares about. Transmit time-stamp seconds.
+    uint32_t txTm_f;         // 32 bits. Transmit time-stamp fraction of a second.
+
+} ntp_packet;              // Total: 384 bits or 48 bytes.
+
+
+int main( int argc, char* argv[ ] ){
+    int serverfd; // socket fd
+
+    struct sockaddr_in servaddr, clientaddr;
+    socklen_t addrlen = sizeof(servaddr);
+    bzero(&servaddr, sizeof(servaddr));
+
+    
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(NTP_PORT_NUMBER);
+
+    // Create a udp socket
+    serverfd = socket( AF_INET, SOCK_DGRAM, IPPROTO_UDP );
+    if (serverfd < 0)
+        error("Error creating socket");
+
+    // Bind the server address to the socket descriptor
+    if (bind(serverfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+        error("Could not bind to address");
+
+}
